67a52fe5ed01778581a43bceb1934ee376ac98e1
Dave Williams <dave@dave.io>
2019-03-11 01:31:38 +0000

    Twiddle build scripts to use Bundler 2.0.1

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+gem update --system
+gem update
+gem install bundler:2.0.1
+bundle update --bundler
 bundle exec jekyll build
 bundle exec htmlproofer ./_site
